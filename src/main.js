4e32fa06e8908f401817376be5dcb3b916421690
kurehajime <xiidec@gmail.com>
2022-03-24 22:39:06 +0900

    説明のためにコード修正

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import peg_js from "./peg_js.mjs";
 import * as evaluter from "./evaluter.mjs";
-const parser = peggy.generate(peg_js)
 
 document.querySelector("#run").addEventListener('click', event => {
     document.querySelector("#result").value = ""
     const code = document.querySelector("#code").value
+    const parser = peggy.generate(peg_js)
     const parsed = parser.parse(code)
     const _evaluter = new evaluter.Evaluter()
     const _env = {native_functions:{print:print},values:{}}
